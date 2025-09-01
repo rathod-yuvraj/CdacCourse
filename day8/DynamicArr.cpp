acd5b5c38738c0a3e3d229ea64ade26fc552cf15
rathod yuvraj <yuvrajrathod8390@gmail.com>
2025-09-01 19:18:44 +0530

    Update Day 8

--- day8/DynamicArr.cpp
+++ day8/DynamicArr.cpp
@@ -2,5 +2,21 @@
 using namespace std;
 
 class Student{
+    int roll;
+    char name[];
+    public:
+    void accept();
+    void display();
+    ~Student();
     
+};
+void Student :: accept(){
+    cout<<"Enter the Name: ";
+    cin>>name;
+     cout<<"Enter the Roll Number";
+    cin>>roll;
+}
+void Student ::display(){
+    cout<<"Name Of Student:"<<name<<endl;
+    cout<<"Roll Numver of Student: "<<roll<<endl;
 }
