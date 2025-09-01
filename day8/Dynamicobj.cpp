8b2318a829a9f382e8b8eabd965705215be86913
rathod yuvraj <yuvrajrathod8390@gmail.com>
2025-09-01 19:06:20 +0530

    day8

--- day8/Dynamicobj.cpp
+++ day8/Dynamicobj.cpp
@@ -5,9 +5,33 @@ class cdate{
 
     int dd,mm,yy;
     public:
-    cdate();
-    cdate(int ,int,int);
-    void show();
+     cdate();
+     cdate(int a ,int b,int c);
+     void show();
+    
 
 
 };
+
+cdate::cdate(){
+    dd=mm=yy=0;
+
+}
+cdate ::cdate(int a,int b,int c){
+    dd=a;
+    mm=b;
+    yy=c;
+}
+void cdate :: show(){
+    cout<<dd<<"/"<<mm<<"/"<<yy<<endl;
+}
+int main(){
+    // cdate c(1,9,2025);
+    // // cdate c();
+    // c.show();
+
+    // using pointer
+    cdate *ptr=new cdate(31,8,2025);
+    ptr->show();
+    delete ptr;
+}
