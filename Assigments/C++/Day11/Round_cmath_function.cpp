3abff2a22d5e987f0f0798ecfe360e33c15dd309
Rathod Yuvraj <145740317+rathod-yuvraj@users.noreply.github.com>
2025-09-11 20:19:05 +0530

    Revert "Rathod"

--- Assigments/C++/Day11/Round_cmath_function.cpp
+++
@@ -1,8 +0,0 @@
-#include<iostream>
-using namespace std;
-#include<cmath>
-int main()
-{
-    float f1=10.5;
-    cout<<"Now the value of f1 is"<<round(f1);
-}
--- Assigments/C++/day6/string/lengthstr.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include<string.h>
-using namespace std;
-
-int main(){
-   string str = "yuvraj";
-    int n=str.length();
-     cout<<"Lenght Of String "<<n<<endl;
-}
