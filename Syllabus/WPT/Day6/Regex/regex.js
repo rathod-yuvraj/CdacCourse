cd4c26dd5857a7c5429451e05596a3c741a08475
rathod yuvraj <yuvrajrathod8390@gmail.com>
2025-09-18 17:34:07 +0530

    update

--- Syllabus/WPT/Day6/Regex/regex.js
+++
@@ -1,9 +0,0 @@
-var text = "Visit to Center ";
-var n = text.search(/to Center/i);
-
-console.log(n);
-
-var text = " to Center Visit to Center ";
-var n = text.match(/to Center/g);
-
-console.log(n);
