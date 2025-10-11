dad033d526e751ce8134bc01d70b8dc528b82864
rathod yuvraj <yuvrajrathod8390@gmail.com>
2025-10-11 21:01:52 +0530

    day10

--- Lab/WPT/React/assign/src/App.js
+++ Lab/WPT/React/assign/src/App.js
@@ -1,11 +1,45 @@
 
-import Home from './Home';
-import Calculator from './Calculator';
-import CaseConverter from './Caseconcls' 
+import Home from './component/Home';
+import AboutUs from './component/AboutUs';
+import Counter from './component/Counter';
+import Contact from './component/Contact';
+import Suyog from './component/Suyog';
+
+import { BrowserRouter,Routes, Route, Link } from 'react-router-dom';
 function App() {
   return (
     <div>
- <Calculator/>
+      <BrowserRouter>
+
+    <nav>
+      <Link to="/">Home</Link>
+       <Link to="/about">About</Link>
+         <Link to="/counter">Counter</Link>
+         <Link to="/yog">Yog</Link>
+    </nav>
+    <Routes>
+
+          <Route path="/" element={
+            <Home/>
+          }/>
+
+         
+           <Route path="/about" element={
+           <AboutUs/>
+          }/>
+           <Route path="/counter" element={
+            <Counter/>
+      
+          }/>
+            <Route path="/contact" element={
+            <Contact/>
+           }/>
+           <Route path="/yog" element={<Suyog/>}
+           />
+        
+        </Routes>
+         </BrowserRouter>
+
     </div>
   );
 }
--- Lab/WPT/React/assign/src/component/Home.js
+++ Lab/WPT/React/assign/src/component/Home.js
@@ -1,14 +1,13 @@
-import React from "react";
+
+
+
 function Home(){
- const str="yuvraj"
+
     return(
         <div>
             <h1>Home</h1>
-            <h1>{str}</h1>
         </div>
-            
-        
-    );
+    )
 }
 
 export default Home;
