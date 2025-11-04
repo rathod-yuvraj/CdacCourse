2e55ac0dd04deca9097b6646ceeae8ec6b846ce4
rathod-yuvraj <yuvrajrathod8390@gmail.com>
2025-11-04 12:05:13 +0530

    Day05

--- Lab/git/C++/HOME/cpp/cpp/Day11/Ceil_function.cpp
+++
@@ -1,8 +0,0 @@
-#include<iostream>
-using namespace std;
-#include<cmath>
-int main()
-{
-    float f=7.8;
-    cout<<"Final value of f is "<<ceil(f);
-}
--- Lab/git/C++/HOME/cpp/cpp/Day2/area_of_rec.cpp
+++
@@ -1,8 +0,0 @@
-#include<iostream>
-using namespace std;
-int main()
-{
-    int l=10,b=20,area;
-    area=l*b;
-    cout<<"Area of rectangle is "<<area;
-}
