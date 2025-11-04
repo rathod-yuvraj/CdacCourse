1446366a4bb7a374d773cc854f2fe5df9c3e784b
rathod-yuvraj <yuvrajrathod8390@gmail.com>
2025-11-04 15:23:30 +0530

    Day06 DSA Notes

---
+++ Lab/DSA&Algorithim/Day05/LInkedlist/src/com/yuvraj/beans/MytestLinked.java
@@ -0,0 +1,17 @@
+package com.yuvraj.beans;
+
+import com.yuvraj.beans.MyLinkedList.Node;
+
+public class MytestLinked {
+
+	public static void main(String[] args) {
+		
+		MyLinkedList olist= new MyLinkedList();
+		int x=10;
+		
+		Node head=new Node(x);
+		
+		
+	}
+	
+}
