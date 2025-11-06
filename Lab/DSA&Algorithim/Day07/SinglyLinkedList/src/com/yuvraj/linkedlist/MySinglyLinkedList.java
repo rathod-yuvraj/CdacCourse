8f94401eaa3b1237dc4124ae37bae0de9786ca59
rathod-yuvraj <yuvrajrathod8390@gmail.com>
2025-11-06 17:23:16 +0530

    Updata day06

--- Lab/DSA&Algorithim/Day07/SinglyLinkedList/src/com/yuvraj/linkedlist/MySinglyLinkedList.java
+++ Lab/DSA&Algorithim/Day07/SinglyLinkedList/src/com/yuvraj/linkedlist/MySinglyLinkedList.java
@@ -15,5 +15,41 @@ public class MySinglyLinkedList {
 		this.head = null;
 	}
 	
+	
+	public void  addData(int value) {
+		Node newNode=new Node(value);
+		Node temp=head;
+		if(head==null) {
+			head=newNode;
+		}
+		else {
+			while(temp!=null) {
+				
+				temp=temp.next;
+			}
+			temp.next=newNode;
+		}
+	}
+	public void displayAll() {
+		
+		Node temp=head;
+		if(temp==null) {
+			System.out.println("Empty List");
+		}
+		while(temp!=null) {
+			System.out.println(temp.data+"-->");
+			temp=temp.next;
+		}
+		
+		
+	}
+public static void main(String[] args) {
+		MySinglyLinkedList a=new MySinglyLinkedList();
+		a.addData(10);
+		a.addData(30);
+		a.displayAll();
+
+	}
+
 
 }
