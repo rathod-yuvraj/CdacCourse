b15ba77a6014d89856ebe24a3b0c2ab6ea8f48ce
rathod-yuvraj <yuvrajrathod8390@gmail.com>
2025-11-11 17:10:11 +0530

    Day12 SA

--- Lab/DSA&Algorithim/Day11/MyBinarySearchTree/src/MyBinarySearchTree.java
+++ Lab/DSA&Algorithim/Day11/MyBinarySearchTree/src/MyBinarySearchTree.java
@@ -1,4 +1,4 @@
-package com.demo.trees;
+
 
 public class MyBinarySearchTree {
     Node root;
@@ -6,8 +6,155 @@ public class MyBinarySearchTree {
     	int data;
     	Node left;
     	Node right;
+		public Node(int data){
+			
+		
+			
+			this.data = data;
+			this.left = null;
+			this.right = null;
+		}
     	
+    }
+    
+    public MyBinarySearchTree() {
+    	root =null;
+    }
+		
+    public void insertNode(int key) {
+    	root = insertData(root,key);
+    }
+    private Node insertData(Node root,int key) {
+    	Node newNode = new Node (key);
+    	//tree is empty,terminating for recursion
+    	if(root == null) {
+    		root = newNode;
+    		return root;
+    		
+    	}else {
+    		if(key<root.data) {
+    			root.left = insertData(root.left,key);
+    			
+    		}else {
+    			root.right = insertData(root.right,key);
+    		}
+    		return root;
+    	}
+    }
+    
+    public void inorder() {
+    	inorderTraversal(root);
+    	System.out.println();
+    	
+    }
+    
+    private void inorderTraversal(Node root) {
+    if(root!=null) {
+    	inorderTraversal(root.left);
+    	System.out.println(root.data+",");
+    	inorderTraversal(root.right);
+    	
+    }
+    
+    }
+    
+    public void postorder() {
+    	postorderTraversal(root);
+    	System.out.println();
+    }
+
+	private void postorderTraversal(MyBinarySearchTree.Node root2) {
+		if(root!=null) {
+			postorderTraversal(root.left);
+			postorderTraversal(root.right);
+			System.out.println(root.data+",");
+		}
+		
+	}
 	
+	public void preorder() {
+    	preorderTraversal(root);
+    	System.out.println();
+    }
+
+	private void preorderTraversal(MyBinarySearchTree.Node root2) {
+		if(root!=null) {
+			preorderTraversal(root.left);
+			preorderTraversal(root.right);
+			System.out.println(root.data+",");
+		}
+		
+	}
+	
+	public void deleteNode(int key) {
+		root = deleteData(root,key);
+		
+	}
+
+	private Node deleteData (Node root, int key) {
+		if(root ==null) {
+			return root;
+		}
+		// traverse to find node
+		if(key<root.data) {
+			root.left = deleteData(root.left,key);
+		}else if(key>root.data) {
+			root.right = deleteData(root.right,key);
+		}else {
+			//found node to delete 1.node is leaf node
+			if(root.left == null && root.right == null) {
+			return null;	
+			}
+			
+			//2.node has 1 child
+			else if (root.left == null) {
+				return root.right;
+				
+			}else if(root.right == null) {
+				return root.left;
+			}
+			
+			//the node has 2 children 
+			root.data =minvalue(root.right);
+			root.right = deleteData(root.right,root.data);
+			
+		}
+		return root;
+		
+		//
+	}
+	
+	private int minvalue(Node root) {
+	int min = root.data;
+	while(root.left!=null) {
+	min = root.left.data;
+	root = root.left;
+		}
+		return min;
+	
+}
+	public boolean search(int key) {
+	return searchBinaryTree(root,key);
+}
+    
+	private boolean searchBinaryTree(Node root, int key) {
+		if(root!=null) {
+			if(root.data == key) {
+				System.out.println(key + "data found");
+				return true;
+				
+			}else if(key<root.data) {
+				return  searchBinaryTree(root.left,key);
+				
+			}else {
+				return  searchBinaryTree(root.right,key);
+			}
+		}
+			
+		System.out.println(key+"Data is not found");	
+		return false;
+	}	
+}
     	
   
 	
--- Lab/DSA&Algorithim/Day11/MyBinarySearchTree/src/MyTestBinarySearchTree.java
+++ Lab/DSA&Algorithim/Day11/MyBinarySearchTree/src/MyTestBinarySearchTree.java
@@ -1,4 +1,26 @@
 
+
 public class MyTestBinarySearchTree {
 
+	public static void main (String [] args ) {
+		MyBinarySearchTree bst = new  MyBinarySearchTree(); {
+			
+			bst.insertNode(4);
+			bst.insertNode(5);
+			bst.insertNode(6);
+			bst.insertNode(8);
+			bst.insertNode(9);
+			bst.insertNode(3);
+			bst.insertNode(3);
+			bst.insertNode(2);
+			bst.insertNode(2);
+			System.out.println("Inorder");
+			bst.inorder();
+			
+		
+		
+		}
+		}
+
+	
 }
